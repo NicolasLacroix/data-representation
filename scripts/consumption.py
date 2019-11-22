48b99b33f069c7db091c68a17b66885a947040df
NicolasLACROIX <ln803631@iutnice.unice.fr>
2019-11-22 22:36:02 +0100

    Daily stats improvements

--- scripts/consumption.py
+++ scripts/consumption.py
@@ -1,30 +1,24 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import numpy as np
+import matplotlib.dates as mdates
 # TODO: visualize the daily consumption
 # TODO: add stats analysis using scipy
 # (https://docs.scipy.org/doc/scipy/reference/stats.html)
 
 
-def visualize(data, x, y, king='line'):
-    ax = data.plot(kind='line', x=x, y=y)
-    # Set the x-axis label
-    ax.set_xlabel("Date")
-    # Set the y-axis label
-    ax.set_ylabel("Consumption (MW)")
+def visualize(data, x, y, kind='line'):
+    ax = data.plot(subplots=True, kind=kind, x=x, y=y)
     plt.show()
 
 
 def getDailyData(data):
     # TODO: get daily values per hour
     res = {}
-    print(data.groupby(['Date']).mean())
-    for value in data.groupby(['Date']).mean().itertuples():
-        pass  # print(value)
-    # for row in data.groupby(['Date']).mean():
-
-        # res[''] =
-        # pass
+    for date, values in data.groupby(['Date']):
+        # day_values = values[['Date - Heure', 'Consommation (MW)']]
+        res[date] = values[['Date - Heure', 'Consommation (MW)']]
+    return res
 
 
 def getExtremums(data, key):
@@ -36,15 +30,24 @@ def getData(filepath, delimiter=';'):
     return pd.read_csv(filepath, delimiter=delimiter)
 
 
+def normalize(data):
+    data = data.values
+    uni_train_mean = data.mean()
+    uni_train_std = data.std()
+    data = (data - uni_train_mean) / uni_train_std
+
+
 def main():
     fp = '../Datasets/ProdConso/eco2mix-regional-tr.csv'
     data = getData(fp)
 
     print(getExtremums(data, 'Consommation (MW)'))
 
-    getDailyData(data)
+    dailyData = getDailyData(data)
 
-    visualize(data, 'Date', 'Consommation (MW)')
+    print(dailyData)
+    visualize(dailyData['2019-10-01'], 'Date - Heure',
+              'Consommation (MW)')
 
 if __name__ == '__main__':
     main()
